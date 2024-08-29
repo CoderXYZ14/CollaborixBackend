96a24959a4021af7e6aa711353a2a0b9411f447e
Shahwaiz Islam <s1404islam@gmail.com>
2024-08-29 07:09:53 +0530

    User Schema created

---
+++ src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+      index: true,
+    },
+    profilePhoto: {
+      type: String,
+    },
+    friendList: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Friend",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
