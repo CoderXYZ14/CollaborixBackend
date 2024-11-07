8f820737c85adf9fa43131cbcd9a2c2e63503fe6
CoderXYZ14 <s1404islam@gmail.com>
2024-11-07 19:09:26 +0530

    socket connection added

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const io = initializeSocket(server);
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    server.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port ${process.env.PORT}`);
     });
   })
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,16 +1,47 @@
 import { Server } from "socket.io";
+import ACTIONS from "../utils/socket-actions/action.js";
 
 export function initializeSocket(server) {
-  const io = new Server(server, {
-    cors: {
-      origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
-  });
+  const io = new Server(
+    server
+    //    {
+    //   cors: {
+    //     origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    //     methods: ["GET", "POST"],
+    //     credentials: true,
+    //   },
+    // }
+  );
+  const userSocketMap = {}; //better to use redis here
+
+  function getAllConnectedCLients(roomId) {
+    // returns all clients of socket id
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || [])
+      .map((socketId) => {
+        return {
+          socketId,
+          username: userSocketMap[socketId],
+        };
+      })
+      .filter(({ username }) => username !== undefined);
+  }
 
   io.on("connection", (socket) => {
-    console.log(`User connected: ${socket.id}`);
+    // console.log(`User connected: ${socket.id}`);
+    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+      userSocketMap[socket.id] = username;
+      socket.join(roomId);
+
+      const clients = getAllConnectedCLients(roomId);
+      //console.log(clients);
+      clients.forEach(({ socketId }) => {
+        io.to(socketId).emit(ACTIONS.JOINED, {
+          clients,
+          username,
+          socketId: socket.id,
+        });
+      });
+    });
   });
 
   return io;
