fe6b0f044588070597eaa50c65b9dd5c7af12eff
CoderXYZ14 <s1404islam@gmail.com>
2025-03-16 23:46:36 +0530

    frontend url added

--- src/app.js
+++ src/app.js
@@ -9,7 +9,10 @@ const app = express();
 // Allow access from any origin
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Use environment variable or default
+    origin:
+      process.env.FRONTEND_URL ||
+      "https://collaborix-phi.vercel.app/" ||
+      "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
@@ -22,14 +25,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// Add OPTIONS handling for preflight requests
-app.options("*", cors()); // Enable pre-flight for all routes
+app.options("*", cors());
 
-// Routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/problems", problemRouter);
 
-// Default route
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 export { app };
